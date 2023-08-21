6a0354f7276ce4a0020aa1fdf482f8e13525c57e
lfkdsk <liufengkai@bytedance.com>
2023-08-21 18:27:56 +0800

    feat: add general config.

--- build.py
+++ build.py
@@ -3,12 +3,24 @@ import os
 import pathlib
 import shutil
 
-base_url = os.getenv("BASE_URL")
-if not base_url:
-    raise "need set base url in github action."
-
 if not os.path.exists("./gallery/"):
     raise "need git clone gallery first."
+
+if not os.path.exists("./gallery/CONFIG.yml") or not os.path.exists('./gallery/README.yml'):
+    raise "CONFIG or README is null."
+
+with open("./gallery/CONFIG.yml", 'r', encoding="utf-8") as g, open("./_config.yml", "r+", encoding="utf-8") as c, open("./new_config.yml", "w", encoding="utf-8") as n:
+    g_file, c_file = yaml.safe_load(g), yaml.safe_load(c)
+    for item in g_file:
+        print(item)
+        c_file[str(item)] = g_file[item]
+        if item == 'url':
+            base_url = f'{g_file[item]}/raw/master'
+    print(list(c_file))        
+    yaml.safe_dump(c_file, n, allow_unicode=True)
+
+if not base_url:
+    raise "need set base url in github action."
 
 with open("./gallery/README.yml", 'r') as f:
     y = yaml.safe_load(f)
